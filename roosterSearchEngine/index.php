70c4e3591d8a702c34523612e24f43309f763cd6
Ivan <ivanaguilar01@live.com.mx>
2021-01-26 20:57:08 -0600

    Función de busqueda facetada

--- roosterSearchEngine/index.php
+++ roosterSearchEngine/index.php
@@ -21,7 +21,7 @@
   <div class="keyword-wrapper">
     <input type="checkbox" id="facet" name="facet" value="true">
     <label for="facet">Busqueda facetada: </label>
-    <input id="facet-input" type="text" autocomplete="off" placeholder="Ingresa el campo..." />
+    <input id="facet-input" name="facet-keyword" type="text" autocomplete="off" placeholder="Ingresa el campo..." />
   </div> 
   <ul id="filter-select" class="filter-select no-value"></ul>
 </form>
@@ -30,12 +30,14 @@
 <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script><script  src="./script.js"></script>
 
 <?php
-  if(isset($_POST['keyword'])) {
+  if(isset($_POST['keyword']) && !isset($_POST['facet'])) {
     $keyword = $_POST["keyword"];
     echo search($keyword);
   } 
   if(isset($_POST['facet'])) {
-    echo "hola";
+    $keyword = $_POST["keyword"];
+    $facetKeyword = $_POST["facet-keyword"];
+    echo facetSearch($keyword, $facetKeyword);
   } 
 ?>
 
--- roosterSearchEngine/searchEngine.php
+++ roosterSearchEngine/searchEngine.php
@@ -1,18 +1,20 @@
 <?php
 
+$url = "http://localhost:8983/solr/maincore/select?";
+
+//Desactivar cors
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+    die();
+}
+
 function search($keyword) {
-    //Desactivar cors
-    header('Access-Control-Allow-Origin: *');
-    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-    header("Allow: GET, POST, OPTIONS, PUT, DELETE");
-    $method = $_SERVER['REQUEST_METHOD'];
-    if($method == "OPTIONS") {
-        die();
-    }
-    
     //Hacer petición
-    $ch = curl_init("http://localhost:8983/solr/maincore/select?q=" . $keyword);
+    $ch = curl_init($GLOBALS['url'] . "q=" . $keyword);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
     $response = curl_exec($ch);
@@ -23,15 +25,33 @@ function search($keyword) {
 
     //Transformar respuesta en array
     $responseArray = json_decode($response, true); 
-
     foreach($responseArray["response"]["docs"] as $result) {
         echo 'Result';
         array_key_exists("author", $result) ? var_dump($result["author"][0]) : 'No disponible';
         array_key_exists("description", $result) ? var_dump($result["description"][0]) : 'No disponible';
         array_key_exists("url", $result) ? var_dump($result["url"][0]) : 'No disponible';
     }
-
-
 } 
+
+function facetSearch($keyword, $facetKeyword) {
+    $ch = curl_init($GLOBALS['url'] . "facet.field=" . $facetKeyword . "&facet=on&q=" . $keyword);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+    $response = curl_exec($ch);
+    curl_close($ch);
+    if(!$response) {
+        return false;
+    }
+
+    $responseArray = json_decode($response, true); 
+    foreach($responseArray["facet_counts"]["facet_fields"][$facetKeyword] as $result) {
+        if(is_numeric($result)){
+            echo "Número de apariciones: " . $result;
+        } else {
+            echo $result;
+        }
+        echo "<br>";
+    }
+}
 
 ?>
